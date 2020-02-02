68fe72f513096aa4ed61ce5d3c518d84b5b3caa7
Himanshu Bhardwaj <himbirlasoft>
2020-02-02 17:45:44 +0530

    first cmt

--- textutil/wsgi.py
+++ textutil/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textutil.settings')
 
@@ -19,7 +19,3 @@ application = get_wsgi_application()
 
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
